eba9f89317551bb831e184f8ef8feab88aab128d
Yure Ribeiro <yureribeirocontato@gmail.com>
2023-01-30 11:13:06 -0300

    config vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const path = require('path')
-const port = process.env.PORT || 3000
+const port = 3000
 
 
 const checklistsRouter = require('./src/routes/checklist')
